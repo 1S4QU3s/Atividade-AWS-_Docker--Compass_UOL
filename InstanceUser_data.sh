88c1e67512a83a18f8718304309d6d6fe4c536e8
1S4QU3s <159395767+1S4QU3s@users.noreply.github.com>
2024-03-28 23:12:26 -0300

    InstanceUser_data.sh

---
+++ InstanceUser_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Os comandos abaixo consiste em:
+# Atualizar o sistema 
+sudo yum update
+# Instalar o docker
+sudo yum install docker -y
+# Iniciar o docker
+sudo systemctl start docker
+# habilitar o docker ao iniciar a instância
+sudo systemctl enable docker
+# Habilitar o usuário atual ao grupo do docker
+sudo usermod -aG docker ${USER}
+# curl no docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#Permissões do diretório docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -sL "https://raw.githubusercontent.com/ .....
+#Instalar o nfs
+sudo yum install amazon-efs-utils -y
+# Criar diretório do nfs com as permissões de acesso
+sudo mkdir -m 666 /home/ec2-user/efs
+# Montar o Nfs
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0e05c56d98c014b37.efs.us-east-1.amazonaws.com:/ /home/ec2-user/efs
+# habilitar a montagem automática do Nfs 
+echo "fs-0e05c56d98c014b37.efs.us-east-1.amazonaws.com:/ /home/ec2-user/efs nfs defaults 0 0" >> /etc/fstab
+# Montar todos os arquivos que estiverem no /etc/fstab
+sudo mount -a
+# Criar diretório do docker-compose
+sudo mkdir /home/ec2-user/docker-compose
+
+
